fa6ed8c5d0a66e7038a644e2b69f42c25ea044f2
Harsh Raj <harshraj.hr08@gmail.com>
2026-02-14 22:32:45 +0530

    fixed postgresql database connection to the system

--- app/auth/token.py
+++ app/auth/token.py
@@ -1,8 +1,12 @@
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
+import os 
+from dotenv import load_dotenv
 
-SECRET_KEY = "secret123"  
-ALGORITHM = "HS256"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")  
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7
 
 def create_access_token(data: dict , expires_delta: timedelta | None = None):
